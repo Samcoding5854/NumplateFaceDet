fea9a80aaa674394b36150507582761f9c21ee16
Samcoding5854 <samcoding5854@gmail.com>
2023-12-08 16:21:50 +0530

    Setted up the frontend and made the base code

--- backend/utils.py
+++ backend/utils.py
@@ -1,5 +1,43 @@
-import requests 
+import cv2
+import requests
 import json
+
+
+#### Preproccessing for OCR ####
+
+def grayscale(image):
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    thresh, im_bw = cv2.threshold(gray_image, 210, 230, cv2.THRESH_BINARY)
+    return im_bw
+
+def noise_removal(image):
+    grayscale(image)
+    import numpy as np
+    kernel = np.ones((1, 1), np.uint8)
+    image = cv2.dilate(image, kernel, iterations=1)
+    kernel = np.ones((1, 1), np.uint8)
+    image = cv2.erode(image, kernel, iterations=1)
+    image = cv2.morphologyEx(image, cv2.MORPH_CLOSE, kernel)
+    image = cv2.medianBlur(image, 3)
+    return (image)
+
+def thick_font(image):
+    import numpy as np
+    # image = cv2.bitwise_not(image)
+    # kernel = np.ones((5,5),np.uint8)
+    # image = cv2.dilate(image, kernel, iterations=1)
+    # image = cv2.bitwise_not(image)
+    return (image)
+
+def final_processed(img):
+    img_bw = grayscale(img)
+
+    rotated_image = cv2.resize(img, None, fx=3, fy=3, interpolation=cv2.INTER_CUBIC)
+    no_noise = noise_removal(rotated_image)
+    dilated_image = thick_font(no_noise)
+
+    return dilated_image
+
 
 
 def ocr_space_file(filename, overlay=True, api_key='K84842733188957', language='eng'):
